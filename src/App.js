ce875b1207705773d2905ff6599f1b98424cc4aa
Eetu <99178022+E3TU@users.noreply.github.com>
2024-01-16 15:40:09 +0200

    Temperature increase&decrease logic

--- src/App.js
+++ src/App.js
@@ -1,16 +1,39 @@
 import './App.css';
+import React, { useState } from 'react';
 
 function App() {
+
+  const [temperatureValue, setTemperatureValue] = useState(10);
+  const [temperatureColor, setTemperatureColor] = useState('cold');
+
+  let increaseTempereture = () => {
+    const newTemperature = temperatureValue + 1;
+		setTemperatureValue(newTemperature);
+
+    if (newTemperature >= 15) {
+			setTemperatureColor('hot');
+		}
+  }
+
+  let decreaseTemperature = () => {
+    const newTemperature = temperatureValue - 1;
+		setTemperatureValue(newTemperature);
+
+    if (newTemperature < 15) {
+			setTemperatureColor('cold');
+		}
+  }
+
   return (
     <div className="app-container">
       <div className="container">
-        <div className="temp-display">
-          10C
+        <div className={`temp-display ${temperatureColor}`}>
+          {temperatureValue}°C
         </div>
       </div>
       <div className="button-container">
-        <button>-</button>
-        <button>+</button>
+        <button onClick={decreaseTemperature}>-</button>
+        <button onClick={increaseTempereture}>+</button>
       </div>
     </div>
   );
